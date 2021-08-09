76b136c3f4e69e7c305c2dfe2d9b3a8f407df820
osama adel mahdy <osamaadelmahdy@gmail.com>
2021-08-09 23:09:54 +0200

    add post from database

--- celint/src/components/feed/Feed.jsx
+++ celint/src/components/feed/Feed.jsx
@@ -1,15 +1,32 @@
+import { useEffect, useState } from "react";
 import Post from "../post/Post";
 import Share from "../share/share";
 import "./feed.css";
-import { Posts } from "../../data";
+import axios from "axios";
 function Feed() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const userPosts = await axios.get(
+        "http://localhost:8080/api/posts/timeline/610965cc80088f2a74ee5c2d"
+      );
+
+      console.log(userPosts.data);
+      setPosts(userPosts.data);
+    };
+    fetchPosts();
+  }, []);
   return (
     <div className="feed">
       <div className="feedWrapper">
         <Share />
-        {Posts.map((post) => (
-          <Post key={post.id} post={post} />
-        ))}
+
+        {posts ? (
+          posts.map((post) => <Post key={post._id} post={post} />)
+        ) : (
+          <h1>Loading...</h1>
+        )}
       </div>
     </div>
   );
--- celint/src/components/post/Post.jsx
+++ celint/src/components/post/Post.jsx
@@ -1,63 +1,78 @@
 import { MoreVert } from "@material-ui/icons";
+import { useEffect, useState } from "react";
 import "./post.css";
-import { Users } from "../../data";
-import { useState } from "react";
+import axios from "axios";
+import { format } from "timeago.js";
 
 export default function Post({ post }) {
-  const [like, setLike] = useState(post.like);
+  const [like, setLike] = useState(post.likes.length);
   const [isLiked, setIsLiked] = useState(false);
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const userData = await axios.get(
+        `http://localhost:8080/api/users/${post.userId}`
+      );
+      setUser(userData.data);
+      console.log(userData.data);
+    };
+    fetchUser();
+  }, []);
+
   const likeHandler = () => {
     setLike(isLiked ? like - 1 : like + 1);
     setIsLiked(!isLiked);
   };
 
-  const x = Users.filter((u) => {
-    return u.id === 1;
-  });
-  console.log(x[0].username);
+  const PostV = () => {};
   return (
     <div className="post">
       <div className="postWrapper">
-        <div className="postTop">
-          <div className="postTopLeft">
-            <img
-              src={Users.filter((u) => u.id === post.userId)[0].profilePicture}
-              alt=""
-              className="postProfileImg"
-            />
-            <span className="postUserName">
-              {Users.filter((u) => u.id === post.userId)[0].username}
-            </span>
-            <span className="postDate">{post.date}</span>
-          </div>
-          <div className="postTopRight">
-            <MoreVert />
-          </div>
-        </div>
-        <div className="postCenter">
-          <span className="postText">{post.desc}</span>
-          <img className="postImg" src={post?.photo} alt="" />
-        </div>
-        <div className="postBottom">
-          <div className="postBottomLeft">
-            <img
-              src="/assets/like.png"
-              alt=""
-              className="likeIcon"
-              onClick={likeHandler}
-            />
-            {/* <img
+        {user ? (
+          <>
+            <div className="postTop">
+              <div className="postTopLeft">
+                <img
+                  src={user.profilePicture || "/assets/no-avatar.jpg"}
+                  alt=""
+                  className="postProfileImg"
+                />
+                <span className="postUserName">{user.username}</span>
+                <span className="postDate">{format(post.createdAt)}</span>
+              </div>
+              <div className="postTopRight">
+                <MoreVert />
+              </div>
+            </div>
+            <div className="postCenter">
+              <span className="postText">{post.desc}</span>
+              <img className="postImg" src={post?.img} alt="" />
+            </div>
+            <div className="postBottom">
+              <div className="postBottomLeft">
+                <img
+                  src="/assets/like.png"
+                  alt=""
+                  className="likeIcon"
+                  onClick={likeHandler}
+                />
+                {/* <img
               src="/assets/heart.png"
               alt=""
               className="likeIcon"
               onClick={likeHandler}
             /> */}
-            <span className="postLikeCounter">{like} pepole like it</span>
-          </div>
-          <div className="postBottomRight">
-            <span className="postCommentText">{post.comment} Comments</span>
-          </div>
-        </div>
+                <span className="postLikeCounter">{like} pepole like it</span>
+              </div>
+              <div className="postBottomRight">
+                <span className="postCommentText">{post.comment} Comments</span>
+              </div>
+            </div>
+          </>
+        ) : (
+          <h4>Loading...</h4>
+        )}
       </div>
     </div>
   );
